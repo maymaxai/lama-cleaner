0bacbdc1c120ab40f6b3f13ef9e80dbe275c6e50
maymaxai <131622579+maymaxai@users.noreply.github.com>
2023-04-25 23:00:25 +0300

    Update win_config.bat

--- scripts/user_scripts/win_config.bat
+++ scripts/user_scripts/win_config.bat
@@ -2,9 +2,9 @@
 
 set PATH=C:\Windows\System32;%PATH%
 
-@call installer\Scripts\activate.bat
+@call miniconda3\Scripts\activate.bat
 
-@call conda-unpack
+
 
 @call conda install -y -c conda-forge cudatoolkit=11.7
 @call pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 --extra-index-url https://download.pytorch.org/whl/cu117
